b888353b24735fe848cdeb7beded6662ffde47e1
Trevor Bača <trevor.baca@gmail.com>
2019-05-16 14:25:34 -0500

    Configured wrapper directory.

--- setup.py
+++ setup.py
@@ -2,18 +2,29 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires = (
-            'abjad',
-            ),
+        install_requires=install_requires,
+        keywords=keywords,
         name='harmony',
-        packages=(
-            'harmony',
-            ),
+        packages=['harmony'],
+        platforms='Any',
         url='https://github.com/trevorbaca/harmony',
-        version='0.1',
-        zip_safe=False,
         )
