4621528e2c6e5ee4bddc4a2fad54e9cc3cc07a8b
Trevor Bača <trevor.baca@gmail.com>
2019-05-16 14:17:51 -0500

    Added metadata.

--- harmony/__metadata__.py
+++ harmony/__metadata__.py
@@ -3,7 +3,9 @@ import abjad
 
 metadata = abjad.OrderedDict(
     [
+        ('catalog_number', 'AWN-020'),
         ('composer', 'Bača'),
+        ('forces_tagline', 'for narrator \& nine players'),
         ('title', '(HARMONY)'),
         ('year', 2019),
         ]
