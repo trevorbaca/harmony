bcc94c3166c063ec0899522738e803631ad30a38
Trevor Bača <trevor.baca@gmail.com>
2019-05-16 14:27:21 -0500

    Configured package initializers.

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -1,6 +1,14 @@
-# import order: tools, materials, segments.
-# makes it possible for materials to import tools.
-# makes it possible for segments to import both.
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.6') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.6.')
+del distutils
+del platform
 from harmony.tools import *
-from harmony.materials import *
+#from harmony.materials.instruments.definition import instruments
+#from harmony.materials.metronome_marks.definition import metronome_marks
 from harmony import segments
+
