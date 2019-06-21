17e0d5c1cfc9ffbd8d681f1907084e253893036f
Trevor Bača <trevor.baca@gmail.com>
2019-06-21 10:21:52 -0500

    Cleaned up masking commands.

--- harmony/tools/bfl_transition_rhythm.py
+++ harmony/tools/bfl_transition_rhythm.py
@@ -4,9 +4,8 @@ from abjadext import rmakers
 
 
 def bfl_transition_rhythm(
-    *,
+    *specifiers,
     divisions: abjad.IntegerSequence = (1, 4, 3, 2),
-    dmask: rmakers.MasksTyping = rmakers.silence([2], period=3),
     extra_counts: abjad.IntegerSequence = (2, 3),
     rotation: int = 0,
 ) -> baca.RhythmCommand:
@@ -18,11 +17,14 @@ def bfl_transition_rhythm(
     return baca.rhythm(
         divisions=baca.divisions().fuse().split(divisions_, cyclic=True),
         rhythm_maker=rmakers.TaleaRhythmMaker(
+            *specifiers,
             rmakers.TupletSpecifier(
-                denominator=(1, 16), extract_trivial=True, force_fraction=True
+                denominator=(1, 16),
+                extract_trivial=True,
+                force_fraction=True,
+                rewrite_rest_filled=True,
             ),
             rmakers.BeamSpecifier(beam_each_division=True),
-            division_masks=dmask,
             extra_counts_per_division=extra_counts,
             talea=rmakers.Talea(counts=[1], denominator=16),
         ),
