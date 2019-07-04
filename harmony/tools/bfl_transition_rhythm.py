f5bf9b1663f8fbe73ec85d4372a4309f56535461
Trevor Bača <trevor.baca@gmail.com>
2019-07-04 16:29:43 -0400

    Cleaned up rhythm construction.

--- harmony/tools/bfl_transition_rhythm.py
+++ harmony/tools/bfl_transition_rhythm.py
@@ -15,7 +15,6 @@ def bfl_transition_rhythm(
     divisions_ = baca.sequence([(_, 4) for _ in divisions])
     divisions_ = divisions_.rotate(n=rotation)
     return baca.rhythm(
-        divisions=baca.divisions().fuse().split(divisions_, cyclic=True),
         rhythm_maker=rmakers.TaleaRhythmMaker(
             *specifiers,
             rmakers.BeamSpecifier(selector=baca.tuplets()),
@@ -25,6 +24,7 @@ def bfl_transition_rhythm(
                 force_fraction=True,
                 rewrite_rest_filled=True,
             ),
+            divisions=baca.divisions().fuse().split(divisions_, cyclic=True),
             extra_counts_per_division=extra_counts,
             talea=rmakers.Talea(counts=[1], denominator=16),
         ),
