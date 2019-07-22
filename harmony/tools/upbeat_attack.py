4da569bfa0584f126ca59490a742845f536cd4f1
Trevor Bača <trevor.baca@gmail.com>
2019-07-22 11:49:55 -0400

    Cleaned up rhythm construction.

--- harmony/tools/upbeat_attack.py
+++ harmony/tools/upbeat_attack.py
@@ -8,14 +8,14 @@ def upbeat_attack() -> baca.RhythmCommand:
     Makes upbeat attack rhythm.
     """
     return baca.rhythm(
-        rmakers.incised(
-            rmakers.extract_trivial(),
-            incise=rmakers.Incise(
+        rmakers.rhythm(
+            rmakers.incised(
                 fill_with_rests=True,
                 suffix_talea=[3, 1],
                 suffix_counts=[2],
                 talea_denominator=16,
             ),
+            rmakers.extract_trivial(),
         ),
         tag="harmony.upbeat_attack",
     )
