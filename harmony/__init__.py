6f49178d16a03494e9a20f8cec5c68612e5d29d0
Trevor Bača <trevor.baca@gmail.com>
2019-08-02 09:18:39 -0400

    Versioned Python to 3.7.

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -2,10 +2,10 @@ import distutils.version
 import platform
 
 if not (
-    distutils.version.LooseVersion("3.6")
+    distutils.version.LooseVersion("3.7")
     < distutils.version.LooseVersion(platform.python_version())
 ):
-    raise ImportError("Requires Python 3.6.")
+    raise ImportError("Requires Python 3.7.")
 del distutils
 del platform
 from harmony.tools import *
