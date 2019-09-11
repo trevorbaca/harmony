79d2f044b52c548692cce3dceeef3f357a297e0b
Trevor Bača <trevor.baca@gmail.com>
2019-09-11 16:00:20 -0400

    AA.

--- harmony/segments/AA/definition.py
+++ harmony/segments/AA/definition.py
@@ -79,13 +79,62 @@ maker(
 
 # bfl
 
+maker(
+    ("bfl", (4, 5)),
+    harmony.appoggiato([7, 6], [9, 6]),
+    baca.text_spanner(
+        "air =|",
+        abjad.tweak(5.5).staff_padding,
+        autodetect_right_padding=True,
+        bookend=False,
+        selector=baca.leaves().rleak(),
+    ),
+)
+
+maker(
+    ("bfl", 13),
+    harmony.sixteenths([4, -99]),
+    baca.markup(r"\baca-jet-whistle-markup", literal=True),
+)
+
 # perc1
+
+maker(
+    ("perc1", (4, 5)),
+    baca.make_repeat_tied_notes(),
+    harmony.brake_drum_staff_position(),
+    baca.markup(r"\baca-brake-drum-paper-towel-markup", literal=True),
+    baca.dynamic("p"),
+)
+
+maker(
+    ("perc1", 13),
+    baca.make_repeat_tied_notes(),
+    harmony.brake_drum_staff_position(),
+    baca.markup(r"\baca-brake-drum-paper-towel-markup", literal=True),
+    baca.dynamic("p"),
+)
 
 # perc2
 
 maker(
     ("perc2", 1),
     baca.staff_lines(3),
+    harmony.sixteenths([4, -20, 4]),
+    baca.accent(baca.pheads()),
+    baca.markup(r"\baca-purpleheart-markup", literal=True),
+    baca.dynamic("f"),
+)
+
+maker(
+    ("perc2", (4, 5)),
+    harmony.sixteenths(
+       [2],
+       beam=True,
+       do_not_rewrite_meter=True,
+       preprocessor=baca.sequence(),
+    ),
+    baca.accent(baca.pheads()),
 )
 
 maker(
@@ -95,12 +144,148 @@ maker(
 
 # hp
 
+maker(
+    ("hp", 1),
+    harmony.sixteenths([-24, 4]),
+    baca.accent(baca.pheads()),
+    baca.laissez_vibrer(baca.ptails()),
+    baca.markup(r"\baca-pince-markup", literal=True),
+    baca.dynamic("mp"),
+)
+
+maker(
+    ("hp", (4, 5)),
+    harmony.sixteenths([4, -99]),
+    baca.accent(baca.pheads()),
+    baca.laissez_vibrer(baca.ptails()),
+    baca.markup(r"\baca-pince-markup", literal=True),
+    baca.dynamic("mp"),
+)
+
 # va
+
+maker(
+    ("va", 1),
+    baca.make_notes(),
+    baca.bow_speed_spanner(
+        "XFB =|",
+        abjad.tweak(3).staff_padding,
+    ),
+    baca.dynamic("mp"),
+)
+
+maker(
+    ("va", (4, 5)),
+    baca.make_notes(),
+    baca.bow_speed_spanner(
+        "XFB =|",
+        abjad.tweak(3).staff_padding,
+    ),
+    baca.dynamic("mp"),
+)
+
+maker(
+    ("va", 13),
+    baca.make_notes(),
+    baca.bow_speed_spanner(
+        "XFB =|",
+        abjad.tweak(3).staff_padding,
+    ),
+    baca.dynamic("mp"),
+)
 
 # vc1
 
+maker(
+    ("vc1", 4),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], stop=4),
+)
+
+maker(
+    ("vc1", 5),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2]),
+)
+
+maker(
+    ("vc1", 13),
+    baca.clef("percussion"),
+    baca.staff_lines(1),
+    baca.make_notes(),
+    baca.staff_position(0),
+    baca.dynamic("p"),
+)
+
 # vc2
+
+maker(
+    ("vc2", 4),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], r=-2, stop=4),
+)
+
+maker(
+    ("vc2", 5),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], r=-2),
+)
+
+maker(
+    ("vc2", 13),
+    baca.make_notes(),
+    baca.staff_position(0, not_yet_pitched=True),
+    baca.dynamic("p"),
+    baca.damp_spanner(
+        abjad.tweak(3).staff_padding,
+    ),
+)
 
 # cb1
 
+maker(
+    ("cb1", 4),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], r=-4, stop=4),
+)
+
+maker(
+    ("cb1", 5),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], r=-4),
+)
+
+maker(
+    ("cb1", 13),
+    baca.clef("percussion"),
+    baca.staff_lines(1),
+    baca.make_notes(),
+    baca.staff_position(0),
+    baca.dynamic("p"),
+)
+
 # cb2
+
+maker(
+    ("cb2", 4),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], r=-6, stop=4),
+)
+
+maker(
+    ("cb2", 5),
+    harmony.sixteenths([6, -2, 4, -2, 4, -2, 2, -2], r=-6),
+)
+
+maker(
+    ("cb2", 13),
+    baca.make_notes(),
+    baca.staff_position(0, not_yet_pitched=True),
+    baca.dynamic("p"),
+    baca.damp_spanner(
+        abjad.tweak(3).staff_padding,
+    ),
+)
+
+# vc1, vc2, cb1, cb2
+
+maker(
+    (["vc1", "vc2", "cb1", "cb2"], (4, 5)),
+    baca.staff_position(0, not_yet_pitched=True),
+    baca.damp_spanner(
+        abjad.tweak(3).staff_padding,
+    ),
+)
