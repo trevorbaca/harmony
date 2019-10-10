1b711050c34912583c636a1c0ef79d4cfd4e906c
Trevor Bača <trevor.baca@gmail.com>
2019-10-10 10:14:03 -0400

    R.

--- harmony/segments/R/__persist__.py
+++ harmony/segments/R/__persist__.py
@@ -84,7 +84,7 @@ persist = abjad.OrderedDict(
                             abjad.Momento(
                                 context='Bass_Flute_Music_Voice',
                                 prototype='abjad.Dynamic',
-                                value='p',
+                                value='niente',
                                 ),
                             ],
                         ),
