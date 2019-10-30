69088368c891b3f2e78963161024e5494673d1e5
Trevor Bača <trevor.baca@gmail.com>
2019-10-30 08:27:36 -0400

    Parts.

---
+++ harmony/builds/letter-parts/__metadata__.py
@@ -0,0 +1,10 @@
+import abjad
+
+
+metadata = abjad.OrderedDict(
+    [
+        ('catalog_number_suffix', 'LET'),
+        ('paper_size', 'letter'),
+        ('parts_directory', True),
+        ]
+    )
