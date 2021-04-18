e89e9435b6629a9bb52fb201da700e23cc4eaaf4
Trevor Bača <trevor.baca@gmail.com>
2021-04-18 14:46:24 -0400

    Cleaned up selectors.

--- harmony/library.py
+++ harmony/library.py
@@ -414,8 +414,7 @@ def phjc(
 
     def preprocessor(argument):
         argument = baca.Sequence(argument).fuse().quarters().partition(divisions)
-        argument = argument.map(lambda _: baca.Sequence(_).flatten().fuse())
-        return argument
+        return baca.Sequence(baca.Sequence(_).flatten().fuse() for _ in argument)
 
     commands: typing.List[rmakers.Command] = []
     if rest is not None:
@@ -449,8 +448,8 @@ def phjc(
         rmakers.denominator((1, 8)),
         rmakers.force_fraction(),
         rmakers.force_repeat_tie((1, 8)),
-        rmakers.force_rest(baca.plts().map(baca.leaves()[1:])),
-        rmakers.force_rest(baca.tuplets().map(baca.leaf(0))),
+        rmakers.force_rest(baca.leaves_in_each_plt(1, None)),
+        rmakers.force_rest(baca.leaf_in_each_tuplet(0)),
         rmakers.beam(),
         rmakers.extract_trivial(),
         frame=inspect.currentframe(),
@@ -625,7 +624,7 @@ def sixteenths(
         repeat_tie_ = rmakers.repeat_tie(selector)
         commands.append(repeat_tie_)
     if tie_runs is True:
-        selector = baca.runs().map(baca.leaves()[1:])
+        selector = baca.leaves_in_each_run(1, None)
         repeat_tie_ = rmakers.repeat_tie(selector)
         commands.append(repeat_tie_)
     if untie is True:
@@ -637,7 +636,7 @@ def sixteenths(
         unbeam_ = rmakers.unbeam(selector)
         commands.append(unbeam_)
     if after_graces:
-        selector = baca.runs().map(baca.leaf(-1))
+        selector = baca.leaf_in_each_run(-1)
         beam_and_slash = None
         if after_graces != [1]:
             beam_and_slash = True
@@ -852,7 +851,7 @@ def warble(
         selector = baca.tuplets().get(*rest_tuplets_cyclic)
         force_rest_ = rmakers.force_rest(selector)
         rests.append(force_rest_)
-    selector = baca.tuplets().map(baca.leaves()[:1])
+    selector = baca.leaf_in_each_tuplet(0)
     force_rest = rmakers.force_rest(selector)
     rests.append(force_rest)
     return baca.rhythm(
