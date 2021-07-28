23422ef2622aaf15fc3a1a915bf8c7a2d4e46f5d
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:49:44 -0400

    Taught package initializer to check for Python 3.9 or later.

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -1,5 +1,4 @@
-import distutils.version
-import platform
+import sys
 
 from .library import (
     ScoreTemplate,
@@ -85,10 +84,6 @@ __all__ = [
     "whisk_staff_position",
 ]
 
-if not (
-    distutils.version.LooseVersion("3.7")
-    < distutils.version.LooseVersion(platform.python_version())
-):
-    raise ImportError("Requires Python 3.7.")
-del distutils
-del platform
+if sys.version_info[:2] < (3, 9):
+    raise ImportError("Requires Python 3.9 or later")
+del sys
