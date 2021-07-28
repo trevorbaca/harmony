970c9f0dbaaa73f577fd4f93e6d2ed37bf555943
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:31:06 -0400

    Cleaned up setup.py file.

--- setup.py
+++ setup.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python
 import setuptools
 
-install_requires = [
-    "abjad",
-    "black",
-    "flake8",
-    "isort",
-    "pytest",
-]
-
-keywords = [
-    "abjad",
-    "lilypond",
-    "music composition",
-    "music notation",
-]
-
 if __name__ == "__main__":
     setuptools.setup(
         author="Trevor Bača",
         author_email="trevor.baca@gmail.com",
-        install_requires=install_requires,
-        keywords=", ".join(keywords),
+        install_requires=[
+            "abjad",
+            "black",
+            "flake8",
+            "isort",
+            "pytest",
+        ],
+        keywords="abjad, lilypond, music composition, music notation",
         name="harmony",
         packages=["harmony"],
         platforms="Any",
+        python_requires=">=3.9",
         url="https://github.com/trevorbaca/harmony",
     )
