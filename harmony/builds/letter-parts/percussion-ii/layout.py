3787cd8906561daf1158590dce81144ff87a7bd8
Trevor Bača <trevor.baca@gmail.com>
2021-08-08 18:23:58 -0400

    Cleaned up layout files.

--- harmony/builds/letter-parts/percussion-ii/layout.py
+++ harmony/builds/letter-parts/percussion-ii/layout.py
@@ -90,7 +90,7 @@ breaks = baca.breaks(
     ),
 )
 
-spacing = baca.scorewide_spacing(
+spacing = baca.spacing(
     __file__,
     breaks=breaks,
     fallback_duration=(1, 32),
--- harmony/builds/letter-parts/viola/layout.py
+++ harmony/builds/letter-parts/viola/layout.py
@@ -89,7 +89,7 @@ breaks = baca.breaks(
     ),
 )
 
-spacing = baca.scorewide_spacing(
+spacing = baca.spacing(
     __file__,
     breaks=breaks,
     fallback_duration=(1, 32),
