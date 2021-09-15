cce35a9b4285a743a092dcb52e3008d0f0a4fd63
Trevor Bača <trevor.baca@gmail.com>
2021-09-15 14:45:07 +0200

    Cleaned up empty score construction.

--- harmony/library.py
+++ harmony/library.py
@@ -1092,7 +1092,7 @@ class ScoreTemplate(baca.ScoreTemplate):
         site = "harmony.ScoreTemplate.__call__()"
         tag = abjad.Tag(site)
         # GLOBAL CONTEXT
-        global_context = self._make_global_context()
+        global_context = baca.templates.make_global_context()
 
         # BASS FLUTE
         bass_flute_music_voice = abjad.Voice(name="Bass_Flute_Music_Voice", tag=tag)
@@ -1287,9 +1287,9 @@ class ScoreTemplate(baca.ScoreTemplate):
 
         # SCORE
         score = abjad.Score([global_context, music_context], name="Score", tag=tag)
-        self._assert_lilypond_identifiers(score)
-        self._assert_unique_context_names(score)
-        self._assert_matching_custom_context_names(score)
+        baca.templates.assert_lilypond_identifiers(score)
+        baca.templates.assert_unique_context_names(score)
+        baca.templates.assert_matching_custom_context_names(score)
         return score
 
     ### PUBLIC PROPERTIES ###
