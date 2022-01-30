09c29f433c87da6ef7ba87ff79557dde2ab0adcb
Trevor Bača <trevor.baca@gmail.com>
2022-01-30 16:53:50 +0100

    Bumped Python to 3.10.

--- harmony/__init__.py
+++ harmony/__init__.py
@@ -4,6 +4,6 @@ from . import library
 
 __all__ = ["library"]
 
-if sys.version_info[:2] < (3, 9):
-    raise ImportError("Requires Python 3.9 or later")
+if sys.version_info[:2] < (3, 10):
+    raise ImportError("Requires Python 3.10 or later")
 del sys
