4edba444b908c00e09d8102a46867222a0a26070
Trevor Bača <trevor.baca@gmail.com>
2022-05-31 15:24:05 +0200

    Cleaned up music.py files.

--- harmony/sections/10/music.py
+++ harmony/sections/10/music.py
@@ -20,7 +20,6 @@ score = library.make_empty_score()
 voice_names = baca.accumulator.get_voice_names(score)
 
 commands = baca.CommandAccumulator(
-    **baca.section_accumulation_defaults(),
     instruments=library.instruments(),
     margin_markups=library.margin_markups(),
     metronome_marks=library.metronome_marks(),
