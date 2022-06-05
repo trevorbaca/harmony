dd4535c9c3180080e37e3a704c20872414379de1
Trevor Bača <trevor.baca@gmail.com>
2022-06-05 19:58:51 +0200

    Cleaned up music.py files.

--- harmony/sections/06/music.py
+++ harmony/sections/06/music.py
@@ -45,6 +45,16 @@ commands = baca.CommandAccumulator(
     voice_names=voice_names,
 )
 
+baca.interpret.set_up_score(
+    score,
+    commands.manifests(),
+    commands.time_signatures,
+    append_anchor_skip=True,
+    always_make_global_rests=True,
+    attach_nonfirst_empty_start_bar=True,
+    stage_markup=stage_markup,
+)
+
 commands(
     "Skips",
     baca.metronome_mark("48", lambda _: baca.select.skip(_, 1 - 1)),
@@ -2081,7 +2091,6 @@ if __name__ == "__main__":
         fermata_measure_empty_overrides=[2, 9],
         global_rests_in_topmost_staff=True,
         parts_metric_modulation_multiplier=(0.525, 0.525),
-        stage_markup=stage_markup,
         transpose_score=True,
     )
     lilypond_file = baca.make_lilypond_file(
