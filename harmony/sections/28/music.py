26ed814a6d8c25212536c50156a5fe47c8bc5b1f
Trevor Bača <trevor.baca@gmail.com>
2022-08-16 08:03:08 -0400

    Bumped LilyPond to 2.23.11.

--- harmony/sections/28/music.py
+++ harmony/sections/28/music.py
@@ -881,7 +881,7 @@ def main():
     VC2(accumulator.voice("vc2"))
     CB1(accumulator.voice("cb1"))
     CB2(accumulator.voice("cb2"))
-    previous_persist = baca.previous_metadata(__file__, file_name="__persist__")
+    previous_persist = baca.previous_persist(__file__)
     baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
     cache = baca.interpret.cache_leaves(
         score,
