ebfdefef340802fd71ef8440b8e08c0cbe0e151b
Trevor Bača <trevor.baca@gmail.com>
2022-08-17 09:39:32 -0400

    Cleaned up music.py files

--- harmony/sections/23/music.py
+++ harmony/sections/23/music.py
@@ -829,7 +829,12 @@ def main():
     CB1(accumulator.voice("cb1"))
     CB2(accumulator.voice("cb2"))
     previous_persist = baca.previous_persist(__file__)
-    baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
+    previous_persistent_indicators = previous_persist["persistent_indicators"]
+    baca.reapply_new(
+        accumulator.voices(),
+        accumulator.manifests(),
+        previous_persistent_indicators,
+    )
     cache = baca.interpret.cache_leaves(
         score,
         len(accumulator.time_signatures),
