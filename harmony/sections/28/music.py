41851bc1dd841a8f8ea65d1bd5f75f9a82a44e5a
Trevor Bača <trevor.baca@gmail.com>
2022-08-19 09:21:46 -0400

    Cleaned up music.py files.

--- harmony/sections/28/music.py
+++ harmony/sections/28/music.py
@@ -922,7 +922,7 @@ if __name__ == "__main__":
             *baca.tags.short_instrument_name_color_tags(),
             baca.tags.RHYTHM_ANNOTATION_SPANNER,
         ),
-        fermata_measure_empty_overrides=[6],
+        empty_fermata_measures=True,
         global_rests_in_topmost_staff=True,
         parts_metric_modulation_multiplier=(0.525, 0.525),
         transpose_score=True,
