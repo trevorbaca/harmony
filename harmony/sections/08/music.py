2e830baa8a47cfd10e8fd939b4f38db2a45e7d4e
Trevor Bača <trevor.baca@gmail.com>
2022-08-19 21:00:44 -0400

    Cleaned up music.py files.

--- harmony/sections/08/music.py
+++ harmony/sections/08/music.py
@@ -33,9 +33,9 @@ accumulator = baca.CommandAccumulator(
 
 first_measure_number = baca.interpret.set_up_score(
     score,
-    accumulator,
     library.manifests,
     accumulator.time_signatures,
+    accumulator,
     append_anchor_skip=True,
     always_make_global_rests=True,
     attach_nonfirst_empty_start_bar=True,
