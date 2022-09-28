a03f1097ae3349de3a49b2c5a826b9d0420df8b7
Trevor Bača <trevor.baca@gmail.com>
2022-09-28 07:54:02 -0400

    Cleaned up music.py files.

--- harmony/sections/09/music.py
+++ harmony/sections/09/music.py
@@ -410,7 +410,7 @@ def composites(cache):
             baca.dls_staff_padding(o, 4)
 
 
-@baca.build.timed
+@baca.build.timed("make_score")
 def make_score(first_measure_number, previous_persistent_indicators):
     score, accumulator = make_empty_score()
     first_measure_number = baca.section.set_up_score(
--- harmony/sections/12/music.py
+++ harmony/sections/12/music.py
@@ -1211,7 +1211,7 @@ def composites(cache):
             baca.tuplet_bracket_up(o)
 
 
-@baca.build.timed
+@baca.build.timed("make_score")
 def make_score(first_measure_number, previous_persistent_indicators):
     score, accumulator = make_empty_score()
     first_measure_number = baca.section.set_up_score(
