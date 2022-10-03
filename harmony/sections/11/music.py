cd409fdb52cc0bd3f79a72594a3eb9d85b34ad02
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 06:17:43 -0400

    Cleaned up timing.

--- harmony/sections/11/music.py
+++ harmony/sections/11/music.py
@@ -1195,11 +1195,10 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
     score, measures = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
-        timing,
+        environment.timing,
     )
     metadata, persist = baca.section.postprocess_score(
         score,
@@ -1220,7 +1219,6 @@ def main():
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
         parts_metric_modulation_multiplier=(0.525, 0.525),
-        timing=timing,
         transpose_score=True,
     )
     lilypond_file = baca.lilypond.file(
@@ -1232,7 +1230,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 
