125637d478ebde17fd2628dcd03e81c61ef66d79
Trevor Bača <trevor.baca@gmail.com>
2023-01-13 18:17:29 -0500

    Set python_requires>=3.10.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         name="harmony",
         packages=["harmony"],
         platforms="Any",
-        python_requires=">=3.11",
+        python_requires=">=3.10",
         url="https://github.com/trevorbaca/harmony",
     )
