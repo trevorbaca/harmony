67085504807c58b4e6f5121524fdad89e5e63054
Trevor Bača <trevor.baca@gmail.com>
2023-01-13 17:05:33 -0500

    Bumped Python to 3.11.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         name="harmony",
         packages=["harmony"],
         platforms="Any",
-        python_requires=">=3.10",
+        python_requires=">=3.11",
         url="https://github.com/trevorbaca/harmony",
     )
