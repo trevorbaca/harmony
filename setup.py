cddcc6c075e6eeaa459c6f2134656cae4f9ff4ac
Trevor Bača <trevor.baca@gmail.com>
2023-01-14 14:54:08 -0500

    Cleaned up setup.py.

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ if __name__ == "__main__":
     setuptools.setup(
         author="Trevor Bača",
         author_email="trevor.baca@gmail.com",
-        install_requires=[
-            "abjad",
-            "black",
-            "flake8",
-            "isort",
-            "pytest",
-        ],
+        install_requires=["abjad>=3.15"],
         keywords="abjad, lilypond, music composition, music notation",
         name="harmony",
         packages=["harmony"],
         platforms="Any",
         python_requires=">=3.10",
         url="https://github.com/trevorbaca/harmony",
+        version="3.15",
     )
