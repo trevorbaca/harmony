ef8e0109e5049c280ff074077d362daf2043f492
Trevor Bača <trevor.baca@gmail.com>
2024-04-19 15:36:55 -0400

    Cleaned up music.py files.

--- harmony/sections/15/music.py
+++ harmony/sections/15/music.py
@@ -757,7 +757,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 
 def persist_score(score, environment):
-    metadata = baca.section.postprocess_score(
+    metadata = baca.section.postprocess(
         score,
         environment,
         library.manifests,
