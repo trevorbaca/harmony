6f900a352e626e62faa54eb566c76ecb87519045
Trevor Bača <trevor.baca@gmail.com>
2024-06-17 14:56:20 -0400

    Cleaned up layout files.

--- harmony/sections/19/music.py
+++ harmony/sections/19/music.py
@@ -1217,6 +1217,7 @@ def make_layout(environment):
         environment.metadata["time_signatures"],
         spacing,
         first_measure_number=environment.first_measure_number,
+        has_anchor_skip=environment.metadata["has_anchor_skip"],
     )
 
 
